7baaa08cbff037c387838aafe1190a7bf94bd358
R <R@DESKTOP-5DUICBU>
2017-12-18 19:40:14 +0100

    bubbly

--- scr/bubblesort/main.py
+++ scr/bubblesort/main.py
@@ -4,6 +4,11 @@ Additional information if required and more infos. Complete sentences please.
 """
 
 import tkinter as tk
+from tkinter import Entry
+from tkinter import StringVar
+import time
+import sys
+import os
 
 __author__ = "123456: John Cleese, 654321: Terry Gilliam"  # put your data here
 __copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni"
@@ -13,21 +18,61 @@ __email__ = "your email address"
 
 
 class Bubblesort:
+    x = 100
+    y = 200
 
     def __init__(self):
         self.root = tk.Tk()
+
         self.root.attributes("-topmost", True)  # put the root to foreground
         self.root.configure(bg="light goldenrod yellow")
-        self.canvas = tk.Canvas(self.root, bg="light goldenrod yellow")
-        self.canvas.pack()
+        self.root.title("Bubblesort")
+
+        """
+        self.v = StringVar()
+        self.e = Entry(self.root2, textvariable=self.v)
+        self.e.pack()
+        self.v.set("a default value")
+        self.s = self.v.get()
+        """
+
+        self.canvas = tk.Canvas(self.root, bg="light goldenrod yellow", highlightthickness=0)
+        self.canvas.config(width=self.x, height=self.y)
+        self.canvas.grid(row=1, column=1)
         self.button = tk.Button(self.root, text='Sort', command=self.callback)
-        self.button.pack()
+        self.button.grid(row=2, column=1)
+        self.button_restart = tk.Button(self.root, text='Restart', command=self.restart_program,
+                                        bd=0, bg="light goldenrod yellow",  highlightcolor="red")
+        self.button_restart.grid(row=2, column=3)
+
+        self.button_exit = tk.Button(self.root, text='Exit', command=self.exit,
+                                     bd=0, bg="light goldenrod yellow",  highlightcolor="red")
+        self.button_exit.grid(row=2, column=4)
+
         self.root.mainloop()
 
+    def restart_program(self):
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+
+    def exit(self):
+        sys.exit()
+
     def callback(self):
-        for i in range(50):
-            self.canvas.create_line(5, i * 5, i * 5, 200,
+        count = 5
+        count_2 = 5
+        a_list = [5, 192, 21, 2, 1, 93, 4, 7, 1, 22, 8, 3, 12, 1]
+        for i in range(len(a_list)):
+            self.canvas.create_line(count, self.y, count, self.y - a_list[i],
                                     fill="RoyalBlue1", activefill="tomato")
+
+            self.canvas.update()
+            count += 5
+
+        for j in range(len(a_list)):
+            print(a_list[j])
+            # x0, y0, x1, y1,
+            time.sleep(0.1)
 
 
 def main():
